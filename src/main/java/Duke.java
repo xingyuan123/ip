6bbed215fac8393fd27bea24158c32464ea5ed21
xingyuan <xingyuan100@gmail.com>
2021-08-18 22:21:21 +0800

    level 0 ez

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Greeting();
     }
+
+    public static void Greeting(){
+        System.out.println("_____________________________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("_____________________________________________________________");
+        System.out.println("Bye.Hope to see you again soon!");
+        System.out.println("_____________________________________________________________");
+    }
+
 }
