f73fabb0353eb5259e606aff9760d05d29f0c06b
xingyuan <xingyuan100@gmail.com>
2021-08-25 17:45:30 +0800

    level 2 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        ToDoList l = new ToDoList();
+        greeting();
+        echo(l);
+        bye();
+    }
+
+    public static void greeting(){
         String logo = "      ____        _        \n"
                 + "     |  _ \\ _   _| | _____ \n"
                 + "     | | | | | | | |/ / _ \\\n"
                 + "     | |_| | |_| |   <  __/\n"
                 + "     |____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        greeting();
-        echo();
-        bye();
-
-    }
-
-    public static void greeting(){
         System.out.println("    _____________________________________________________________");
         System.out.println("    Hello! I'm Duke");
         System.out.println("    What can I do for you?");
@@ -22,14 +22,19 @@ public class Duke {
 
     }
 
-    public static void echo(){
+    public static void echo(ToDoList l){
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
-        while(line.equals("bye") == false){
-            System.out.println("    _____________________________________________________________");
-            System.out.println("    "+line);
-            System.out.println("    _____________________________________________________________");
+        while(!line.equals("bye")){
+            if(line.equals("list")){
+                l.printList();
+            } else{
+                System.out.println("    _____________________________________________________________");
+                System.out.println("     added:  "+line);
+                l.addToList(line);
+                System.out.println("    _____________________________________________________________");
+            }
             in = new Scanner(System.in);
             line = in.nextLine();
         }
@@ -39,6 +44,4 @@ public class Duke {
         System.out.println("    Bye.Hope to see you again soon!");
         System.out.println("    _____________________________________________________________");
     }
-
-
 }
